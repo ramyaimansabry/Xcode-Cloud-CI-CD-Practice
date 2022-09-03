ec0c5c4581ce58f40721bf71f927cc8eaa30e7f4
Ramy Sabry <ramysabry1996@gmail.com>
2022-09-03 21:40:17 +0200

    Initial Commit

---
+++ Xcode Cloud Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Xcode Cloud Practice
+//
+//  Created by Ramy Sabry on 03/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
